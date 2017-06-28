9162b2cb082160365e5f5420848f592bbb5fe31c
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-28 16:22:57 +0100

    Added NextBlockComponent

--- src/javafx/GameApp.java
+++ src/javafx/GameApp.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -19,9 +20,16 @@ public class GameApp extends Application {
 		Game game = new Game();
 		KeyHandler controller = new KeyHandler(game);
 		GameGridComponent gameGridComponent = new GameGridComponent(9, 9);
+		NextBlockComponent nextBlockComponent = new NextBlockComponent();
+		HBox hbox = new HBox();
+		hbox.getChildren().add(gameGridComponent);
+		hbox.getChildren().add(nextBlockComponent);
 //		gameGridComponent.addController(controller);
 		game.addObserver(gameGridComponent);
-		Scene scene = new Scene(gameGridComponent, 500, 500);
+		game.addObserver(nextBlockComponent);
+//		Scene scene = new Scene(gameGridComponent, 500, 500);
+//		Scene scene = new Scene(nextBlockComponent, 500, 500);
+		Scene scene = new Scene(hbox, 500, 500);
 		scene.setOnKeyPressed(controller);
 		scene.getStylesheets().add(getClass().getResource("stylesheet.css").toExternalForm());
 		stage.setScene(scene);

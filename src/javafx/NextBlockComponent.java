6a308e0576de47b750e2849be42ea9501aa7a49b
Dom Parfitt <dom.parfitt@cgi.com>
2017-06-28 16:21:52 +0100

    Completed NextBlockComponent

--- src/javafx/NextBlockComponent.java
+++ src/javafx/NextBlockComponent.java
@@ -1,27 +1,28 @@
 package javafx;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
 import core.Game;
 import core.GameGrid;
+import core.Position;
 import javafx.application.Platform;
 import javafx.scene.layout.GridPane;
 import shapes.Shape;
 
 public class NextBlockComponent extends GridPane implements Observer {
-	
-	
+
 	public NextBlockComponent() {
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 6; j++) {
 				add(new EmptySpace(), j, i);
 			}
 		}
-		
+
 	}
 
-	//TODO: Check this
+	// TODO: Check this
 	@Override
 	public void update(Observable obs, Object arg1) {
 		Platform.runLater(new Runnable() {
@@ -30,20 +31,29 @@ public class NextBlockComponent extends GridPane implements Observer {
 			public void run() {
 				Game game = (Game) obs;
 				Shape nextShape = game.getNextShape();
+				int height = nextShape.getHeight();
+				int width = nextShape.getWidth();
+
+				int startingY = (6 - height) / 2;
+				int startingX = (6 - width) / 2;
+
 				GameGrid grid = game.getGrid();
+
+				List<Position> positions = nextShape.getOccupiedSpaces();
+
 				getChildren().clear();
-				for (int i = nextShape.getHeight() - 1; i >= 0; i--) {
-					for (int j = 0; j < nextShape.getWidth(); j++) {
-						if (grid.isOccupied(i, j)) {
-							add(new Block(), j, nextShape.getHeight() - i);
-						} else {
-							add(new EmptySpace(), j, nextShape.getHeight() - i);
-						}
+				for (int i = 0; i < 6; i++) {
+					for (int j = 0; j < 6; j++) {
+						add(new EmptySpace(), j, 5 - i);
 					}
+				}
+
+				for (Position position : positions) {
+					add(new Block(), startingX + position.getX(), 5 - (startingY + position.getY()));
 				}
 			}
 		});
-		
+
 	}
 
 }
